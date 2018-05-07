48304b128f95c78dbdac4e120cc0d4466b99a07c
Tim Filer <tim.j.filer@gmail.com>
2018-05-07 11:27:16 -0500

    Added class and routing for committee listing

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ exports.webPort = 3000;
 exports.httpMsgsFormat = "JSON";
 
 exports.memberSql = "SELECT [Member ID] as memberId, First_Name as firstName, Last_Name as lastName, email, address, address2, city, state, zip, country, telephone, fax_number, webAccess FROM Main ";
-exports.activitySql = "SELECT [Member ID] as memberId, positionCode, actOptOut, memYear, chairman, coChair, termExp, activityNote FROM Activities ";
+exports.activitySql = "SELECT a.[Member ID] as memberId, a.positionCode, a.actOptOut, a.memYear, a.chairman, a.coChair, a.termExp, a.activityNote, m.First_Name	as firstName, m.Last_Name as lastName, m.degrees, m.email FROM Activities a INNER JOIN Main m ON a.[Member ID] = m.[Member ID]";
 exports.activityCodeSql = "SELECT * FROM [Activity Codes] ";
 exports.defaultSearchLimit = 25;
 
