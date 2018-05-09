9fd207e56169e200a26ceaafef87df17155c5da1
Tim Filer <tim.j.filer@gmail.com>
2018-05-09 15:59:26 -0500

    completed field list for member lookup model

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ exports.dbConfig = {
 exports.webPort = 3000;
 exports.httpMsgsFormat = "JSON";
 
-exports.memberSql = "SELECT [Member ID] as id, First_Name as firstName, Last_Name as lastName, middle_nam as middleName, degrees as degree, academicti as academicTitle, department, org as institution, telephone as busPhone, email, address, address2, city, state, zip, country, fax_number as fax, webAccess FROM Main ";
+exports.memberSql = "SELECT [Member ID] as memberId, First_Name as firstName, Last_Name as lastName, middle_nam as middleName, degrees as degree, academicti as academicTitle, department, org as institution, telephone as busPhone, email, address, address2, city, state, zip, country, fax_number as fax, webAccess FROM Main ";
 exports.activitySql = "SELECT a.[Member ID] as memberId, a.positionCode, a.actOptOut, a.memYear, a.chairman, a.coChair, a.termExp, a.activityNote, m.First_Name	as firstName, m.Last_Name as lastName, m.degrees, m.email FROM Activities a INNER JOIN Main m ON a.[Member ID] = m.[Member ID]";
 exports.activityCodeSql = "SELECT * FROM [Activity Codes] ";
 exports.defaultSearchLimit = 25;
